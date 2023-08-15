f8f3e3b87686e7950397d5ce1630c2911c2dc692
gsmkawsar <support@bestflashfile.com>
2023-08-15 22:54:53 +0600

    Withdraw done

--- login.bank.js
+++ login.bank.js
@@ -14,13 +14,35 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     const balance = document.getElementById('balance');
     const totalBalanceintgar = balance.innerText;
     const totalBalance = parseFloat(totalBalanceintgar)
-    
+
     const finalBalance = totalBalance + depositMoney;
     balance.innerText = finalBalance;
 
     deposit.value = ''
 
 
+})
 
 
+document.getElementById('btn-Withdraw').addEventListener('click', function(){
+        const withdraw = document.getElementById("Withdraw-balance");
+        const withdrawBalanceString = withdraw.value;
+        const withdrawBalance = parseFloat(withdrawBalanceString);
+
+        const withdrawAmount = document.getElementById('withdraw');
+        const totalWithdRawing = withdrawAmount.innerText;
+        const totalWithdraw = parseFloat(totalWithdRawing);
+
+        const finalWithdraw = withdrawBalance + totalWithdraw;
+        withdrawAmount.innerText = finalWithdraw;
+
+        const balance = document.getElementById('balance');
+        const totalBalanceintgar = balance.innerText;
+        const totalBalance = parseFloat(totalBalanceintgar)
+
+        const filanBalance = totalBalance - withdrawBalance;
+        balance.innerText = filanBalance;
+
+
+        withdraw.value = ''
 })
